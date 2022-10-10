f0a1b400cb6a2f6d192a727be889bd20fa8610df
Your Name <youremail@domain.com>
2022-10-10 15:42:10 +0100

    Add my_new_script.py

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -1,8 +1,9 @@
 import numpy as np
 from scipy.ndimage import gaussian_filter
 from scipy import misc
+import pandas as pd
 
-__all__ = ['rand_array', 'smooth_image', 'my_mat_solve']
+__all__ = ['rand_array', 'smooth_image', 'my_mat_solve', 'new_function']
 # __all__ = [...]
 
 
@@ -14,3 +15,6 @@ def smooth_image(a, sigma=1):
 
 def my_mat_solve(A, b):
     return A.inv()*b
+
+def new_function(x):
+    return pd.date_range("20130101", periods=x)
---
+++ scripts/my_new_script.py
@@ -0,0 +1,5 @@
+from envtest import new_function
+
+x = new_function(5)
+
+print(x)
